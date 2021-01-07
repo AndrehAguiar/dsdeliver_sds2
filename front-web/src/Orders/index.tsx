6b7252b626926203532fd0d1ffd450d4b3b4aee4
AndrehAguiar <andreaguiar.g@gmail.com>
2021-01-06 22:12:56 -0300

    Add ProductList, ProductCards and axios

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
-import StepHeader from './StepsHeader';
 import './styles.css';
+import { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
+import StepHeader from './StepsHeader';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
