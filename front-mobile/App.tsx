d6f5dacdfb7a32776d9a003dd0218c98bffe0f90
AndrehAguiar <andreaguiar.g@gmail.com>
2021-01-09 22:56:03 -0300

    Add Routes, Orders and OrderCard

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,9 @@
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import React from 'react';
+import Routes from './src/Routes/Routes';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 
 export default function App() {
 
@@ -19,8 +18,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 export default function Home() {
 
-    const handleOnPress = () => {
+    const navigation = useNavigation();
 
+    const handleOnPress = () => {
+        navigation.navigate('Orders');
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 <Image source={require('../assets/deliveryman.png')} />
                 <Text style={styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
@@ -17,7 +22,7 @@ export default function Home() {
             </View>
             <View style={styles.footer}>
                 <RectButton style={styles.button}>
-                    <Text style={styles.buttonText}>VER PEDIDOS</Text>
+                    <Text style={styles.buttonText} onPress={handleOnPress}>VER PEDIDOS</Text>
                 </RectButton>
 
             </View>
